7af5730c67dec2bd4130e0c865384a7238efcd4a
Lorenzo Rossi <lorenzo.rossi@forward3d.com>
2017-12-21 16:52:12 +0000

    Day 17 fixed

--- Day17/Day17.R
+++ Day17/Day17.R
@@ -6,32 +6,29 @@ mypos = 2
 myvalue = 2
 while(myvalue <= 2017){
   mypos = (mypos + input - 1) %% myvalue + 1
-  if(mypos == myvalue){
-    pos_array = c(pos_array, myvalue)
-  }else{
+  if(mypos != myvalue)
     pos_array[(mypos + 2) : (myvalue + 1)] = pos_array[(mypos + 1) : myvalue]
-    pos_array[mypos + 1] = myvalue
-  }
+  pos_array[mypos + 1] = myvalue
   myvalue = myvalue + 1
   mypos = mypos + 1
+  values_in_pos_2 = c(values_in_pos_2, pos_array[2])
 }
 pos_array[which(pos_array == 2017) + 1]
-# 1547
+
+
 ###################### PART 2 ######################
-pos_array = c(0, 1)
-final_array = rep(NA, 2017)
+# It is obvious that 0 will always stay in position 1 since there is no way to move it.
+# Therefore looking for the number after the 0 is simply looking at what is in position 2
+# At this point I do not need to remeber the position of all the elements but just:
+# what is in position 2 and the active position
 mypos = 2
 myvalue = 2
-F3D.Utilities::profiling({
-while(myvalue <= 100000){
-  mypos = (mypos + input - 1) %% length(pos_array) + 1
-  if(mypos == length(pos_array)){
-    pos_array = c(pos_array, myvalue)
-  }else{
-    pos_array = c(pos_array[1:mypos], myvalue, pos_array[(mypos + 1):length(pos_array)])  
-  }
+value_in_pos_2 = 1
+while(myvalue <= 50000000){
+  mypos = (mypos + input - 1) %% myvalue + 1
+  if(mypos == 1)
+    value_in_pos_2 = myvalue
   myvalue = myvalue + 1
   mypos = mypos + 1
 }
-})
-pos_array[which(pos_array == 2017) + 1]
+value_in_pos_2
